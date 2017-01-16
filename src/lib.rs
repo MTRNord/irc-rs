870fed7283c029d86457f6ff841d7fa01589c5b5
Marcel <mtrnord1@gmail.com>
2017-01-16 10:50:30 +0100

    (feature) add port to parameters

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 extern crate bufstream;
 mod irc {
-    fn connect(host: String) {
+    fn connect(host: String, port: u16) {
       use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
       use std::io::Write;
       use bufstream::BufStream;
       let host_split: Vec<&str> = host.split('.').collect();
       let ServerAddress = IpAddr::V4(Ipv4Addr::new(host_split[0].as_bytes()[0], host_split[1].as_bytes()[0], host_split[2].as_bytes()[0], host_split[3].as_bytes()[0])); // Connect to localhost
-      let ServerPort = 6667;
-      let SocketAddress = SocketAddr::new(ServerAddress, ServerPort);
+      let SocketAddress = SocketAddr::new(ServerAddress, port);
       let mut isConnected = false;
 
       let mut stream = BufStream::new(TcpStream::connect(SocketAddress).unwrap());
